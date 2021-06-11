f96a2136a485f69bf81f36026ea90ecaa32f781e
Сергей Дубовой <follow@MacBook-Air-Sergej.local>
2021-06-11 15:08:21 +1000

    added tablesheet class, multidialog tests

---
+++ TableSheet.js
@@ -0,0 +1,70 @@
+TEMPLATE_URL = "https://docs.google.com/spreadsheets/d/1BOdtjztnjySzfZYL8TCwv7_JfFRpXauq37Yru5dJtl0/edit#gid=953649761";
+
+class TableSheet{
+    constructor(name){
+        this.name = name;
+        this.sheet = this.getSheet();
+        this.columns = {};
+        this.getColumns();
+    }
+}
+
+TableSheet.prototype.getSheet = function(){
+    var sheet = SpreadsheetApp.getActive().getSheetByName(this.name);
+    if (sheet != null){
+        return sheet;
+    } else {
+        return this.createSheet(this.name);
+    }
+}
+
+TableSheet.prototype.createSheet = function(name){
+    var ss = SpreadsheetApp.openByUrl(TEMPLATE_URL);
+    var templateSheet = ss.getSheetByName(this.name);
+    return SpreadsheetApp.getActive().insertSheet(this.name, {template: templateSheet});
+}
+
+TableSheet.prototype.getColumns = function(){
+    var col_count = this.sheet.getLastColumn();
+    var names = this.sheet.getSheetValues(1, 1, 1, col_count)[0];
+    for (var i = 0; i < names.length; i ++){
+        this.columns[names[i]] = i;
+    }
+}
+
+TableSheet.prototype.getData = function () {  
+    var rows_count = this.sheet.getLastRow();
+    var columns_count = this.sheet.getLastColumn();
+    return this.sheet.getRange(2, 1, rows_count, columns_count).getValues();
+}
+
+TableSheet.prototype.setValue = function(row_i, col_i, value, note=undefined){
+    var range = this.sheet.getRange(row_i, col_i);
+    range.setValue(value);
+    if (note !== undefined)
+        range.setNote(note);
+}
+
+TableSheet.prototype.appendRow = function(data){
+    var last_row = this.sheet.getLastRow();
+    this.updateRow(data, last_row);
+    return last_row;
+}
+
+TableSheet.prototype.updateRow = function(data, row_i){
+    for (var column_name in data){
+        var index = this.columns[column_name] + 1;
+        this.setValue(row_i, index, data[column_name]);
+    }
+}
+
+
+// class PortfolioSheet extends TableSheet{
+//     constructor(name){
+//         super(name);
+//     }
+// }
+
+// PortfolioSheet.prototype.createSheet = function(){
+
+// }
--- main.js
+++ main.js
@@ -1,17 +1,58 @@
 function onOpen(){
     var ui = SpreadsheetApp.getUi();
     ui.createAddonMenu()
+        .addItem("Настроить документ", "startWorking")
         .addItem("Добавить портфель", "newPortfolio")
         .addToUi()
 }
 
+NECESSARY_TABLES = ["Дашборд", "Портфели", "Тикеры", "Сделки"];
+
+function startWorking(){
+    for (var i in NECESSARY_TABLES){
+        new TableSheet(NECESSARY_TABLES[i]);
+    }
+}
 
 function newPortfolio(){
+    if (!checkExcistingTables()){
+        return;
+    }
+
     var html = HtmlService.createHtmlOutputFromFile('createPortfolio')
         .setWidth(400)
         .setHeight(300);
     SpreadsheetApp.getUi()
         .showModalDialog(html, 'Создать портфель');
+}
+
+function newPortfolio2(data){
+    createNewPortfolio(data);
+    var html = HtmlService.createHtmlOutputFromFile('createPortfolio2')
+        .setWidth(400)
+        .setHeight(300);
+    SpreadsheetApp.getUi()
+        .showModalDialog(html, 'Создать портфель');
+}
+
+function createNewPortfolio(data){
+    var portfolioTS = new TableSheet("Портфели");
+    var options = {
+
+    };
+    
+    // portfolioTS.appendRow(data)
+}
+
+function checkExcistingTables(){
+    var sheets = SpreadsheetApp.getActive().getSheets();
+    for (var i in NECESSARY_TABLES){
+        var filtered = sheets.filter((sheet) => {sheet.getName() == NECESSARY_TABLES[i]});
+        if (filtered.length == 0){
+            errorHandler("Документ не настроен", "Отсутствует таблица "+ NECESSARY_TABLES[i] +". Нажмите на пункт \"Настроить документ\" в выпадающем списке расширения.");
+            return false;
+        }
+    }
 }
 
 function errorHandler(error_type, message){
